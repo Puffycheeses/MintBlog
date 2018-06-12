9323dcb79fb1341ecc02a66af4ab820600d938e8
Puffycheeses <noahheague@gmail.com>
2018-06-12 17:08:20 +1000

    Changed the way posts are styled

--- Scripts/reviewConstruct.js
+++ Scripts/reviewConstruct.js
@@ -3,5 +3,5 @@ function Re(ti, bo, im, de) { // Constructor for a post
     this.bo = bo; // Body
     this.im = im; // Image
     this.de = de; // Destination
-    this.dr = function () {$(this.de).append("<div class=\'post-container\'>\n    <div  class=\'post-thumb\'>\n        <img class=\'artwork\' src=\'"+this.im+"\'>\n    </div>\n    <div class=\"post-content\">\n        <h1 class=\"post-title\">"+this.ti+"</h1>\n        <p>"+this.bo+"</p>\n    </div>\n</div>\n<hr>");} // Draw at Destination
+    this.dr = function () {$(this.de).append("<div class=\'row\'>\n        <div class=\'col-md-3\'>\n            <img class=\"img-responsive artwork\" src=\'"+this.im+"\'>\n        </div>\n        <div class=\'col-md-9\'>\n            <h1 class=\"heading\">"+this.ti+"</h1>\n            <p class=\"body\">"+this.bo+"</p>\n        </div>\n    </div>\n</div>\n<hr>");} // Draw at Destination
 }
